f193aca101c80a7cbe68a7fa34565f6e9e4a635f
Amgad1123 <Amgadahmed2303@gmail.com>
2025-01-06 11:57:47 -0600

    added javascript to tic tac toe to create a more functional game

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let humanScore = 0
+let computerScore = 0
 
 function userChoice() {
     let user = window.prompt("Choose one. Rock, paper or scissors").toLowerCase()
+    console.log(user)
     return user
 }
 function getComputerChoice() {
@@ -15,12 +18,53 @@ function getComputerChoice() {
     else{
         choice = "scissors"
     }
-
+    console.log(choice)
     return choice
 }
 
 function game (userChoice, getComputerChoice) {
+    let winner
+    if (userChoice == "rock") {
+        if (getComputerChoice == "rock") {
+            
+        }
+        else if (getComputerChoice == "paper") {
+             computerScore++;
+        }
+        else {
+            humanScore++;
+        }
+    }
+    else if (userChoice == "paper"){
+        if (getComputerChoice == "rock") {
+            humanScore++
+        }
+        else if (getComputerChoice == "paper") {
+        }
+        else {
+            computerScore++
+        }
+    }
+    else if (userChoice == "scissors") {
+        if (getComputerChoice == "rock") {
+            computerScore++
+        }
+        else if (getComputerChoice == "paper") {
+             humanScore++
+        }
+        else {
+        }
+    }
 
+    if(humanScore > computerScore) {
+        winner = "Human won"
+    }
+    else if (humanScore < computerScore) {
+        winner = "Computer won"
+    }
+
+    console.log(winner)
 }
-console.log(userChoice())
-console.log(getComputerChoice())
+const humanSelection = userChoice()
+const computerSelection = getComputerChoice()
+game(humanSelection, computerSelection)
